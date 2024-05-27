7847441b031abf4e0ccfe6fd8803ab05561fe5f3
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-05-27 23:01:19 +0430

    Add trustedby component to the Home file as a chile

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import Featured from "../../components/featured/Featured";
+import React from 'react';
+import Featured from '../../components/featured/Featured';
+import { TrustedBy } from '../../components/trustedBy/TrustedBy';
 
 const Home = () => {
   return (
     <div>
       <Featured />
+      <TrustedBy />
     </div>
   );
-}
+};
 
 export default Home;
