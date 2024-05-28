8b46c18b5990a6a17fbd87fb59940c0a4bce4014
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-05-28 15:04:47 +0430

    Change the slide component from prop-input to contain children instead

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Featured from "../../components/featured/Featured";
 import { TrustedBy } from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/Slide/Slide";
+import { cards } from "../../temporary/data";
+import CategoryCard from "../../components/categoryCard/CategoryCard";
 
 const Home = () => {
   return (
     <div>
       <Featured />
       <TrustedBy />
-      <Slide />
+      <Slide>
+        {cards.map((item => <CategoryCard item={item} key={item.id} />))}
+      </Slide>
     </div>
   );
 };
