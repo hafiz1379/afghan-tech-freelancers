4f078088144a708dfd42dbbb2da53f8fbed421d4
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-05-28 07:05:50 +0430

    Append the Slide in the Home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Featured from '../../components/featured/Featured';
-import { TrustedBy } from '../../components/trustedBy/TrustedBy';
+import React from "react";
+import Featured from "../../components/featured/Featured";
+import { TrustedBy } from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/Slide/Slide";
 
 const Home = () => {
   return (
     <div>
       <Featured />
       <TrustedBy />
+      <Slide />
     </div>
   );
 };
