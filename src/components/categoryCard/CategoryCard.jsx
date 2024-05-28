899adc5b5cae00dde99be7ceb3ac7cc436b422ce
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-05-28 15:03:14 +0430

    Update the design and style of CategoryCard

--- src/components/categoryCard/CategoryCard.jsx
+++ src/components/categoryCard/CategoryCard.jsx
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 const CategoryCard = ({ item }) => {
   return (
     <Link to="/gigs/123">
-      <div className="w-80 h-80 text-white rounded cursor-pointer relative">
+      <div className="h-80 text-white rounded cursor-pointer relative m-4">
         <img src={item.img} alt={item.title} className="w-full object-cover" />
         {/* Card description */}
-        <span className="font-normal absolute top-4 left-4">{item.desc}</span>
+        <span className="font-normal absolute top-4 left-4 drop-shadow-lg bg-black bg-opacity-80 px-2 rounded-full">
+          {item.desc}
+        </span>
 
         {/* Card Title */}
-        <span className="font-semibold text-lg absolute top-10 left-4">
+        <span className="font-semibold text-lg absolute top-12 left-4 drop-shadow-lg bg-main px-3 rounded-full">
           {item.title}
         </span>
       </div>
