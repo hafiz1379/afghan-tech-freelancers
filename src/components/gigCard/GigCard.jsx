6292e063ff7798858ca2fac097e466b91bbb3d6c
hafiz1379 <hafizrasa1379@gmail.com>
2024-05-29 08:47:11 +0430

    Update GigCard component with responsive styling and icons

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaStar, FaHeart } from "react-icons/fa";
 
 const GigCard = ({ item }) => {
   return (
-    <div className="w-full sm:w-1/2 lg:w-1/4 p-4">
+    <div className="w-full sm:w-1/2 lg:w-1/4 py-4 sm:px-2">
       <Link to="/gig/123">
-        <div className="max-w-xs rounded overflow-hidden shadow-md h-full">
+        <div className=" rounded overflow-hidden shadow-md h-full">
           <img src={item.img} alt="" className="w-full h-40 object-cover" />
           <div className="px-6 py-4">
             <div className="flex items-center mb-2">
@@ -14,13 +15,13 @@ const GigCard = ({ item }) => {
             </div>
             <p className="text-gray-700 text-base">{item.desc}</p>
             <div className="flex items-center mt-4">
-              <img src="./images/star.png" alt="" className="w-4 h-4 mr-1" />
+            <FaStar className="text-yellow-500 w-4 h-4 mr-1" />
               <span className="text-yellow-500">{item.star}</span>
             </div>
           </div>
           <hr className="border-gray-200" />
           <div className="px-6 py-4 flex justify-between items-center">
-            <img src="./images/heart.png" alt="" className="w-6 h-6" />
+          <FaHeart className="text-gray-500 w-6 h-6" />
             <div className="text-right">
               <span className="text-gray-500 text-sm">STARTING AT</span>
               <h2 className="text-lg font-semibold">${item.price}</h2>
