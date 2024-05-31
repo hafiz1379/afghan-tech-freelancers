5eca6374b58c57f4f0212be400f612aa4fd42048
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-05-31 11:05:58 +0430

    Add the write component insed the Message.jsx file

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -20,3 +20,21 @@ const Message = () => {
     </div>
   );
 };
+
+
+const Write = () => {
+  return (
+    <div className="flex flex-col sm:flex-row items-end gap-2 justify-between">
+      <textarea
+        className="h-[100px] border rounded p-3 w-full flex-1"
+        name="write"
+        id="write"
+        placeholder="write a message"
+      ></textarea>
+      <button className="bg-[#1dbf73] px-4 py-1 text-white rounded border-gray-500 border">
+        Send
+      </button>
+    </div>
+  );
+};
+
