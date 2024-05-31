c02b2b2ef6c41ff06b4254cb239ba4c909e0855f
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-05-31 11:06:33 +0430

    Append MessageItem component

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -21,7 +21,6 @@ const Message = () => {
   );
 };
 
-
 const Write = () => {
   return (
     <div className="flex flex-col sm:flex-row items-end gap-2 justify-between">
@@ -38,3 +37,30 @@ const Write = () => {
   );
 };
 
+const MessageItem = ({ isOwner }) => {
+  return (
+    <div
+      className={`flex gap-[2px] sm:gap-2 justify-between flex-col sm:flex-row sm:max-w-[500px] ${
+        isOwner && "ml-auto sm:flex-row-reverse items-end sm:items-start"
+      }`}
+    >
+      <img
+        className="h-8 w-8 lg:h-12 lg:w-12 rounded-full border border-gray-500 p-[1px] object-cover"
+        src="https://avatars.githubusercontent.com/u/76435157?v=4"
+        alt="Reza Merzaee"
+      />
+      <p
+        className={`text-sm md:text-base leading-6 p-5 rounded-2xl  ${
+          isOwner
+            ? "rounded-tr-none bg-blue-200"
+            : "rounded-tl-none bg-gray-200"
+        }`}
+      >
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+        obcaecati nisi nobis ipsam placeat rerum non necessitatibus soluta
+      </p>
+    </div>
+  );
+};
+
+export default Message;
