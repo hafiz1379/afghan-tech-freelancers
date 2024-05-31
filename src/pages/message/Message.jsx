8493497fc7e77a81f438e0e0acd30525c5ec3648
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-05-31 11:05:30 +0430

    Add the message component

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Message = () => {
   return (
-    <div>
-      <h1>Message</h1>
+    <div className="message p-4 lg:p-12">
+      <span className="breadcrumb">
+        <Link to="/messages">Messages</Link> &gt; Reza Merzaee
+      </span>
+      <div className="flex flex-col gap-4 max-w-[900px] mx-auto">
+        <div className="p-10 max-h-[400px] overflow-y-scroll flex flex-col gap-5">
+          <MessageItem isOwner={false} />
+          <MessageItem isOwner={true} />
+        </div>
+
+        <hr className="h-0 border-b border-gray-100 mb-5" />
+
+        <Write />
+      </div>
     </div>
   );
-}
-
-export default Message;
+};
