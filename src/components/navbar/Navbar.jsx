430032293a266bd15a0659e28d58e6b6c4f1ab71
hafiz1379 <hafizrasa1379@gmail.com>
2024-06-01 09:44:32 +0430

    refactor: Update Navbar component styles for responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full bg-nav bg-opacity-80 sticky top-0 z-20">
-      <div className="flex justify-between items-center px-4 sm:px-8 md:px-16 lg:px-32 xl:px-56 h-16">
+      <div className="flex justify-between items-center px-4 sm:px-8 md:px-10 lg:px-24 xl:px-14 h-16">
         {/* Logo */}
         <div className="flex items-center space-x-1">
           <Link to="/">
@@ -40,7 +40,7 @@ const Navbar = () => {
           {!currentUser && <button className="bg-main hover:bg-accent text-white px-4 py-2 rounded-lg">Join</button>}
           {currentUser && (
             <div className="flex gap-2 items-center cursor-pointer relative">
-              <img className="h-8 w-8 rounded-2xl" src="/images/man.png" alt="" />
+              <img className="h-8 w-8 rounded-2xl" src="https://avatars.githubusercontent.com/u/117447018?v=4" alt="Profile picture" />
               <span>{currentUser?.username}</span>
               {open && <div className="absolute top-12 right-0 p-5 bg-white border-gray-500 border-2 rounded-lg flex flex-col gap-2 text-gray-600 w-48 font-light">
                 {
@@ -78,7 +78,7 @@ const Navbar = () => {
           {currentUser && (
             <div>
               <div className="flex gap-2 items-center cursor-pointer" onClick={toggleUserMenu}>
-                <img className="h-8 w-8 rounded-2xl" src="" alt="" />
+                <img className="h-8 w-8 rounded-2xl" src="https://avatars.githubusercontent.com/u/117447018?v=4" alt="Profile picture" />
                 <span>{currentUser?.username}</span>
               </div>
               {isUserMenuOpen && (
