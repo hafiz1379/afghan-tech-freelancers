cd947b2001d238571b4bcfba7f68ffd586facb45
hafiz1379 <hafizrasa1379@gmail.com>
2024-06-27 21:48:27 +0430

    Improve error handling in deleteUser route and add createError utility function

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,12 +1,15 @@
 import User from "../models/user.model.js";
-import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
 
 export const deleteUser = async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-
-    if (req.userId !== user._id.toString()){
+  try {
+    const user = await User.findById(req.params.id);
+    if (req.userId !== user._id.toString()) {
       return next(createError(403, "You can delete only your account"));
     }
     await User.findByIdAndDelete(req.params.id);
     res.status(200).send("User has been deleted");
-  };
+  } catch (error) {
+    return next(createError(404, "User could not be deleted"));
+  }
+};
