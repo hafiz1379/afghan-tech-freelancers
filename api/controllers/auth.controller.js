fe369a8bfb0c810bde86d3609d3c38d6a0d7c722
hafiz1379 <hafizrasa1379@gmail.com>
2024-06-27 13:14:58 +0430

    Add error handling middleware, deleteUser route, and JWT token verification middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import User from "./../models/user.model.js";
+import createError from "../utils/createError.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 // Register a new user
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   try {
     const hash = bcrypt.hashSync(req.body.password, 5);
     const newUser = new User({
@@ -14,32 +15,40 @@ export const register = async (req, res) => {
     await newUser.save();
     res.status(201).send("User has been created");
   } catch (error) {
-    res.status(500).send("Something went wrong");
+    next(error);
   }
 };
 
 // Log in to the user
-export const login = async (req, res) => {
+export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
+
     if (!user)
-      return res.status(404).send("There is not any user with the given info");
+      return next(createError(404, "There is no user with this information"));
 
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
-    if (!isCorrect) res.status(400).send("Wrong password or username");
+    if (!isCorrect) 
+      return next(createError(400, "Wrong password or username"));
 
-    const token = jwt.sign({
-      id: user._id,
-      isSeller: user.isSeller,
-    }, process.env.JWT_KEY)
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
 
     const { password, ...info } = user._doc;
 
-    res.cookie("accessToken", token, {
-      httpOnly: true,
-    }).status(200).send(info);
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
   } catch (error) {
-    res.status(500).send("Something went wrong");
+    next(error)
     console.log(error);
   }
 };
