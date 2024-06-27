6d621e84ec9422376217212479bc23479a0d24a3
hafiz1379 <hafizrasa1379@gmail.com>
2024-06-27 12:08:43 +0430

    Add deleteUser route and JWT token verification middleware

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,8 @@
 import express from "express";
-import {  } from "./../controllers/user.controller.js";
+import { deleteUser } from "./../controllers/user.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
-router.get("/register", );
-router.get("/login", );
+router.delete("/:id",verifyToken, deleteUser);
 
 export default router;
