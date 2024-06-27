d734dbbede9ea4c339b8496f741ef3c99200e88f
hafiz1379 <hafizrasa1379@gmail.com>
2024-06-27 12:08:51 +0430

    Add JWT token verification middleware for authentication

---
+++ api/middleware/jwt.js
@@ -0,0 +1,10 @@
+export const verifyToken = (req, res) => {
+    const token = req.cookies.accessToken;
+    if (!token) return res.status(401).send("You are not authenticated"); 
+
+    jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
+        if (err) return res.status(403).send("Token is not valid");
+        req.userId = payload.id;
+        req.isSeller = payload.isSeller;
+    });
+};
