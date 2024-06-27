ba15a2625d08d3ef39ca191670fc575ab3ac39b2
hafiz1379 <hafizrasa1379@gmail.com>
2024-06-27 12:08:34 +0430

    Add JWT token authentication for login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "./../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // Register a new user
 export const register = async (req, res) => {
@@ -27,9 +28,16 @@ export const login = async (req, res) => {
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
     if (!isCorrect) res.status(400).send("Wrong password or username");
 
+    const token = jwt.sign({
+      id: user._id,
+      isSeller: user.isSeller,
+    }, process.env.JWT_KEY)
+
     const { password, ...info } = user._doc;
 
-    res.status(200).send(info);
+    res.cookie("accessToken", token, {
+      httpOnly: true,
+    }).status(200).send(info);
   } catch (error) {
     res.status(500).send("Something went wrong");
     console.log(error);
