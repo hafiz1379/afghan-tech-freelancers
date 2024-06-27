05c16ace0c216f54a000aa06faaaaba455e1ccf1
hafiz1379 <hafizrasa1379@gmail.com>
2024-06-27 12:08:59 +0430

    Add deleteUser route and JWT token verification middleware

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,12 @@
-export const fn = (req, res) => {
-  res.send("From Controller");
-};
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+export const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+    if (req.userId !== user._id.toString()){
+      return res.status(403).send("You are not allowed to delete this user");
+    }
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send("User has been deleted");
+  };
