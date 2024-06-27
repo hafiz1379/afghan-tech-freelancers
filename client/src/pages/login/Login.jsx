9bf9c16d00ee17e82627bb209616a385d8360394
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-06-27 22:45:32 +0430

    Design the login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,9 +3,41 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <h1>Login</h1>
+      <form className="max-w-[500px] rounded border p-5 mx-auto my-auto mt-5">
+        <h1 className="text-3xl font-semibold mb-2.5">Sign in</h1>
+        <div className="my-2">
+          <label htmlFor="username" className="block">
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            className="block w-full"
+          />
+        </div>
+
+        <div className="my-2">
+          <label className="block" htmlFor="password">
+            Password
+          </label>
+          <input
+            type="text"
+            name="password"
+            id="password"
+            className="block w-full"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-green-400 text-white py-2 w-full my-2 rounded"
+        >
+          Log in
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default Login;
