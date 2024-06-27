acb7d4f62c10236c6d241aa1f80c92f2b80e8c49
hafiz1379 <hafizrasa1379@gmail.com>
2024-06-27 13:15:07 +0430

    Refactor deleteUser route and add error handling middleware

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,11 +1,11 @@
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
-export const deleteUser = async (req, res) => {
+export const deleteUser = async (req, res, next) => {
   const user = await User.findById(req.params.id);
 
     if (req.userId !== user._id.toString()){
-      return res.status(403).send("You are not allowed to delete this user");
+      return next(createError(403, "You can delete only your account"));
     }
     await User.findByIdAndDelete(req.params.id);
     res.status(200).send("User has been deleted");
