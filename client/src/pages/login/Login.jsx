a1882040b7ab81bb94b4c1f2fdfdea3e97614efb
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-06-28 08:14:39 +0430

    Design and implement the login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,31 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import newRequest from "../../utils/newRequest";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await newRequest.post("auth/login", { username, password });
+      console.log(res);
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+      setError(null);
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+    console.log(error);
+  };
+
   return (
     <div>
-      <form className="max-w-[500px] rounded border p-5 mx-auto my-auto mt-5">
+      <form
+        className="max-w-[400px] rounded border p-5 mx-auto my-auto mt-20"
+        onSubmit={handleSubmit}
+      >
         <h1 className="text-3xl font-semibold mb-2.5">Sign in</h1>
-        <div className="my-2">
+        <div className="my-4">
           <label htmlFor="username" className="block">
             Username
           </label>
           <input
             type="text"
             name="username"
+            placeholder="e.g: John"
             id="username"
             className="block w-full"
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
 
-        <div className="my-2">
+        <div className="my-4">
           <label className="block" htmlFor="password">
             Password
           </label>
           <input
-            type="text"
+            type="password"
             name="password"
+            placeholder="Type password"
             id="password"
             className="block w-full"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
@@ -35,6 +64,8 @@ const Login = () => {
         >
           Log in
         </button>
+
+        {error ? <p className="my-2 text-red-500">{error}</p> : <p></p>}
       </form>
     </div>
   );
