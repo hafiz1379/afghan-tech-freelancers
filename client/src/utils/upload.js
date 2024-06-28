6b263829d6422711c99aea9093c5b55c64fb4e12
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-06-28 16:38:42 +0430

    Add other file name

--- client/src/utils/upload.js
+++ client/src/utils/upload.js
@@ -4,14 +4,16 @@ const upload = async (file) => {
   const data = new FormData();
   data.append("file", file);
   data.append("upload_preset", "afghantf");
+  const anwarUsername = "dzfkvkctg";
+  const hafizUsername = "dllyzzjo7";
 
   try {
     const res = await axios.post(
-      "https://api.cloudinary.com/v1_1/dllyzzjo7/image/upload",
-      data
+      `https://api.cloudinary.com/v1_1/${anwarUsername}/image/upload`,
+      data,
     );
 
-    const {url} = res.data;
+    const { url } = res.data;
     return url;
   } catch (err) {
     console.log(err);
