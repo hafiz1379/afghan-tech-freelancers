414066a3cb9fb0577e982bd48cf2b9df064a12a1
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-06-28 18:33:20 +0430

    Add the "createGig" controller

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -1,3 +1,36 @@
-export const fn = (req, res) => {
-  res.send("From Controller");
+import Gig from "../models/gig.model";
+import createError from "../utils/createError";
+
+export const getAllGigs = async (req, res, next) => {
+  try {
+  } catch (error) {}
+};
+export const createGig = async (req, res, next) => {
+  try {
+    if (!req.isSeller) {
+      return next(createError(403, "Only sellers can create gig"));
+    }
+
+    const newGig = new Gig({
+      userId: req.userId,
+      ...req.body,
+    });
+    const savedGig = await newGig.save();
+
+    res.status(201).json(savedGig);
+  } catch (error) {
+    next(error);
+  }
+};
+export const deleteGig = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+export const getGig = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
 };
--- api/models/gig.model.js
+++ api/models/gig.model.js
@@ -66,7 +66,7 @@ const GigSchema = new Schema(
   },
   {
     timestamps: true,
-  }
+  },
 );
 
 export default mongoose.model("Gig", GigSchema);
