50f3e8d42b624970ed996d518057f59e856a8f8f
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-07-01 00:13:06 +0430

    Delete the verify token from the userRoute

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,7 +3,7 @@ import { deleteUser, getUser } from "./../controllers/user.controller.js";
 import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
-router.delete("/:id",verifyToken, deleteUser);
-router.get("/:id",verifyToken, getUser);
+router.delete("/:id", verifyToken, deleteUser);
+router.get("/:id", getUser);
 
 export default router;
