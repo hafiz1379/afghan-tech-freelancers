7fb31ee753b6b23ade6a9b8e81a86273da4a039b
hafiz1379 <hafizrasa1379@gmail.com>
2024-06-30 12:27:06 +0430

    Update GigCard component to use react-query for fetching user data

--- client/src/components/gigCard/GigCard.jsx
+++ client/src/components/gigCard/GigCard.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaStar, FaHeart } from "react-icons/fa";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 const GigCard = ({ item }) => {
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: [item.userId],
+    queryFn: () =>
+      newRequest
+        .get(
+          `/users/${item.userId}`
+        )
+        .then((res) => {
+          return res.data;
+        }),
+  });
+
   return (
     <div className="w-full sm:w-1/2 lg:w-1/4 py-4 sm:px-2">
       <Link to="/gig/123">
         <div className=" rounded overflow-hidden shadow-md h-full">
-          <img src={item.img} alt="" className="w-full h-40 object-cover" />
+          <img src={item.cover} alt="" className="w-full h-40 object-cover" />
           <div className="px-6 py-4">
+           {isLoading ? (
+            "Loading"
+           ) : error ? (
+            "Something went wrong"
+            ) : (
             <div className="flex items-center mb-2">
-              <img src={item.pp} alt="" className="w-10 h-10 rounded-full mr-2" />
-              <span className="text-sm font-semibold">{item.username}</span>
-            </div>
+              <img src={data.img || "https://t4.ftcdn.net/jpg/03/59/58/91/360_F_359589186_JDLl8dIWoBNf1iqEkHxhUeeOulx0wOC5.jpg"} alt="" className="w-10 h-10 rounded-full mr-2" />
+              <span className="text-sm font-semibold">{data.username}</span>
+            </div>)}
             <p className="text-gray-700 text-base">{item.desc}</p>
             <div className="flex items-center mt-4">
             <FaStar className="text-yellow-500 w-4 h-4 mr-1" />
-              <span className="text-yellow-500">{item.star}</span>
+              <span className="text-yellow-500">
+              {!isNaN(item.totalStars /item.starNumber) && 
+              Math.round(item.totalStars /item.starNumber)}
+              </span>
             </div>
           </div>
           <hr className="border-gray-200" />
@@ -24,7 +47,9 @@ const GigCard = ({ item }) => {
           <FaHeart className="text-gray-500 w-6 h-6" />
             <div className="text-right">
               <span className="text-gray-500 text-sm">STARTING AT</span>
-              <h2 className="text-lg font-semibold">${item.price}</h2>
+              <h2 className="text-lg font-semibold">
+                ${item.price}
+                </h2>
             </div>
           </div>
         </div>
