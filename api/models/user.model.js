40db6949db02661f8666a2bbcbd513c572020d82
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-06-30 17:25:01 +0430

    Refactor the code

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,43 +1,46 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     img: {
-        type: String,
-        required: false,
+      type: String,
+      required: false,
     },
     country: {
-        type: String,
-        required: false,
+      type: String,
+      required: false,
     },
     phone: {
-        type: String,
-        required: false,
+      type: String,
+      required: false,
     },
     desc: {
-        type: String,
-        required: false,
+      type: String,
+      required: false,
     },
     isSeller: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-}, {
+  },
+  {
     timestamps: true,
-});
+  },
+);
 
 export default mongoose.model("User", userSchema);
