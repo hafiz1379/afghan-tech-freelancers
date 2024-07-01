630c7bd049ebade8a76a6cdf63260d66febbe846
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-07-01 21:01:20 +0430

    Remove the trusted by section from the home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
-import { TrustedBy } from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/Slide/Slide";
 import { cards } from "../../temporary/data";
 import CategoryCard from "../../components/categoryCard/CategoryCard";
 import Features from "../../components/features/Features";
 
-
 const Home = () => {
   return (
     <div>
       <Featured />
-      <TrustedBy />
       <Slide>
         {cards.map((item) => (
           <CategoryCard item={item} key={item.id} />
