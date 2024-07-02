23f63a197d212fa111c013b75997f86c75a1d81c
hafiz1379 <hafizrasa1379@gmail.com>
2024-07-02 14:02:58 +0430

    Add createOrder and getOrders routes to order.route.js

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,7 +1,10 @@
 import express from "express";
-import {  } from "./../controllers/order.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
+import { getOrders, createOrder } from "../controllers/order.controller.js";
 
 const router = express.Router();
-router.get("/test", );
+
+router.post("/:gigId", verifyToken, createOrder);
+router.get("/", verifyToken, getOrders); 
 
 export default router;
