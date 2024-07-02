6e1f571c7d18bc8b878321f0d52ef22486db3ce4
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-07-02 22:16:20 +0430

    Reformat the conversation model

--- api/models/conversation.model.js
+++ api/models/conversation.model.js
@@ -3,35 +3,35 @@ const { Schema } = mongoose;
 
 const ConversationSchema = new Schema(
   {
-   id: {
-     type: String,
-     required: true,
-     unique: true,
-   },
-   sellerId: {
-    type: String,
-    required: true,
-   },
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    sellerId: {
+      type: String,
+      required: true,
+    },
     buyerId: {
-     type: String,
-     required: true,
+      type: String,
+      required: true,
     },
     readBySeller: {
-     type: Boolean,
-     required: false,
+      type: Boolean,
+      required: false,
     },
     readByBuyer: {
-     type: Boolean,
-     required: false,
+      type: Boolean,
+      required: false,
     },
     lastMessage: {
-        type: String,
-        required: false,
-    }
+      type: String,
+      required: false,
+    },
   },
   {
     timestamps: true,
-  }
+  },
 );
 
 export default mongoose.model("Conversation", ConversationSchema);
