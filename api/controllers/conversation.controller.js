b663f24db371d97d1672031d694aa1e22ba62ebe
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-07-02 22:16:48 +0430

    Create the conversation CRUD controllers

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -1,3 +1,57 @@
-export const fn = (req, res) => {
-  res.send("From Controller");
+import createError from "../utils/createError.js";
+import Conversation from "../models/conversation.model.js";
+
+export const createConversation = async (req, res, next) => {
+  const newConversation = new Conversation({
+    id: req.isSeller ? req.userId + req.body.to : req.body.to + req.userId,
+    sellerId: req.isSeller ? req.userId : req.body.to,
+    buyerId: req.isSeller ? req.body.to : req.userId,
+    readBySeller: req.isSeller,
+    readByBuyer: !req.isSeller,
+  });
+  try {
+    const savedConversation = await newConversation.save();
+    res.status(201).send(savedConversation);
+  } catch (error) {
+    return next(createError(404, "Something went wrong from conversation"));
+  }
+};
+
+export const getAllConversations = async (req, res, next) => {
+  try {
+    const allConversations = await Conversation.find(req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId });
+    res.status(200).send(allConversations);
+  } catch (error) {
+    return next(createError(404, "Something went wrong from conversation"));
+  }
+};
+
+export const getSingleConversation = async (req, res, next) => {
+  try {
+    const conversation = await Conversation.findOne({ id: req.params.id });
+    res.send(200).send(conversation);
+  } catch (error) {
+    return next(createError(404, "Something went wrong from conversation"));
+  }
+};
+
+export const updateConversation = async (req, res, next) => {
+  try {
+    const updatedConversation = await Conversation.findOneAndUpdate(
+      { id: req.params.id },
+      {
+        $set: {
+          readBySeller: req.isSeller,
+          readByBuyer: !req.isSeller,
+        },
+      },
+      {
+        new: true,
+      },
+    );
+
+    res.status(200).send(updateConversation);
+  } catch (error) {
+    return next(createError(404, "Something went wrong from conversation"));
+  }
 };
