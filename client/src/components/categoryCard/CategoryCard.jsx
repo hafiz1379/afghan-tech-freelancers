a44817542755cb4c012758a887124fff7cce9d8e
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-07-07 16:39:33 +0430

    Update category card to be dynamic

--- client/src/components/categoryCard/CategoryCard.jsx
+++ client/src/components/categoryCard/CategoryCard.jsx
@@ -1,18 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CategoryCard = ({ item }) => {
   return (
-    <Link to="/gigs">
+    <Link to={`gigs?cat=${item.title}`}>
       <div className="h-96 text-white rounded-xl m-2 cursor-pointer relative overflow-hidden">
-        <img src={item.img} alt={item.title} className="w-full object-cover" />
-        {/* Card description */}
-        <span className="font-normal absolute top-4 left-4 drop-shadow-lg bg-black bg-opacity-80 px-2 rounded-full">
-          {item.desc}
-        </span>
+        <img src={item.img} alt={item.title} className="w-full object-cover h-full" />
 
-        {/* Card Title */}
-        <span className="font-semibold text-lg absolute top-12 left-4 drop-shadow-lg bg-main px-3 rounded-full">{item.title}</span>
+        <div className="absolute top-2 left-2 right-2">
+          <span className="font-semibold text-lg inline-block mb-3 drop-shadow-lg bg-main px-3 rounded-full">{item.title}</span>
+          <span className="font-normal block drop-shadow-lg bg-black bg-opacity-80 px-2 rounded-full">{item.desc}</span>
+        </div>
       </div>
     </Link>
   );
