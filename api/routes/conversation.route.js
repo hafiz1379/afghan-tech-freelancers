b9d08af9013299eff2a0d14b8609ad34b67d2cb8
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-07-07 16:38:59 +0430

    Fix linters in the files

--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,16 +1,17 @@
-import express from "express";
+/* eslint-disable import/extensions */
+import express from 'express';
 import {
   getAllConversations,
   createConversation,
   getSingleConversation,
   updateConversation,
-} from "./../controllers/conversation.controller.js";
-import { verifyToken } from "./../middleware/jwt.js";
+} from '../controllers/conversation.controller.js';
+import { verifyToken } from '../middleware/jwt.js';
 
 const router = express.Router();
-router.get("/", verifyToken, getAllConversations);
-router.post("/", verifyToken, createConversation);
-router.get("/single/:id", verifyToken, getSingleConversation);
-router.put("/:id", verifyToken, updateConversation);
+router.get('/', verifyToken, getAllConversations);
+router.post('/', verifyToken, createConversation);
+router.get('/single/:id', verifyToken, getSingleConversation);
+router.put('/:id', verifyToken, updateConversation);
 
 export default router;
