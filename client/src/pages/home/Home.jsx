69094dedd8b96caeaa095c95b83a5378358ac743
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-07-07 16:40:30 +0430

    Fetch data from category and display them in the Home

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
 import Slide from "../../components/Slide/Slide";
-import { cards } from "../../temporary/data";
 import CategoryCard from "../../components/categoryCard/CategoryCard";
 import Features from "../../components/features/Features";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
+import Alert from "../../components/alert/Alert";
 
 const Home = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["categories"],
+    queryFn: () => newRequest.get(`categories`).then((res) => res.data),
+  });
+
+  const categories = data?.data?.categories;
+
   return (
     <div>
       <Featured />
       <Slide>
-        {cards.map((item) => (
-          <CategoryCard item={item} key={item.id} />
-        ))}
+        {isPending ? (
+          <Alert message="Loading..." />
+        ) : error ? (
+          <Alert message="Something went wrong" />
+        ) : (
+          categories.map((item) => <CategoryCard item={item} key={item._id} />)
+        )}
       </Slide>
       <Features />
     </div>
