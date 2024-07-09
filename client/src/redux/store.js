b82da3ed6a796c1afada677f0a977ed395a8c0f2
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-07-09 15:04:13 +0430

    Update store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import categorySlice from './categories/categorySlice';
 import gigSlice from './gigs/gigSlice';
-import userSlice from "./users/userSlice";
+import userSlice from './users/userSlice';
+import reviewSlice from './reviews/reviewSlice';
+import messageSlice from './messages/messageSlice';
+import conversationSlice from './conversations/conversationSlice';
+import orderSlice from './orders/orderSlice';
 
 const store = configureStore({
   reducer: {
     categories: categorySlice,
     gigs: gigSlice,
-    users: userSlice
+    user: userSlice,
+    reviews: reviewSlice,
+    messages: messageSlice,
+    conversations: conversationSlice,
+    orders: orderSlice,
   },
 });
 
