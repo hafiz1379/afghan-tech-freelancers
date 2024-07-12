c2061177c4510d7130450f15a85ab69e50ab956d
hafiz1379 <hafizrasa1379@gmail.com>
2024-07-12 10:17:38 +0430

    Add i18n support to Success component and update success message

--- client/src/pages/success/Success.jsx
+++ client/src/pages/success/Success.jsx
@@ -1,33 +1,35 @@
 import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import newRequest from "../../utils/newRequest";
+import { useTranslation } from "react-i18next";
 
 const Success = () => {
+  const { t } = useTranslation();
+  const { search } = useLocation();
+  const navigate = useNavigate();
+  const params = new URLSearchParams(search);
+  const payment_intent = params.get("payment_intent");
 
-    const { search } = useLocation();
-    const navigate = useNavigate();
-    const params = new URLSearchParams(search);
-    const payment_intent = params.get("payment_intent");
-
-    useEffect(() => {
-        const makeRequest = async () => {
-            try {
-                await newRequest.put("orders", { payment_intent });
-                setTimeout(() => {
-                    navigate("/orders");
-                }, 5000);
-            }
-            catch (err) {
-                console.log(err);
-            }
-        };
-        makeRequest();
-    }, [])
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        await newRequest.put("orders", { payment_intent });
+        setTimeout(() => {
+          navigate("/orders");
+        }, 5000);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    makeRequest();
+  }, []);
 
   return (
-    <div>
-        Payment successful!. You are being redirected to the orders page. Please
-        do not close the page.
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="max-w-md px-6 py-4 bg-white shadow-lg rounded-lg text-center">
+        <p className="text-xl font-bold text-green-500 mb-2">{t("paymentSuccess")}</p>
+        <p className="text-gray-700 mb-2">{t("redirectMessage")}</p>
+      </div>
     </div>
   );
 };
