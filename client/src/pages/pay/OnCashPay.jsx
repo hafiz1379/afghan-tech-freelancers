0ee78d2aff1686a2dd121335c9a6411ffec14330
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-07-15 09:43:44 +0430

    Add the onCashPay

--- client/src/pages/pay/OnCashPay.jsx
+++ client/src/pages/pay/OnCashPay.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function OnCashPay() {
-  return <div>OnCashPay</div>;
+  const [documents, setDocuments] = useState();
+  const handleSubmit = () => {};
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-[600px] p-5 my-3 rounded border mx-auto"
+      >
+        <div>Include</div>
+      </form>
+    </div>
+  );
 }
 
 export default OnCashPay;
