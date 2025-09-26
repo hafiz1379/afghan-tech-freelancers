aa9bbbe766e6063d9f4fa522e2b6bfdcfb754890
hafiz1379 <hafizrasa1379@gmail.com>
2025-09-26 13:43:57 +0430

    updated

--- api/server.js
+++ api/server.js
@@ -74,14 +74,14 @@ const connect = async () => {
   }
 };
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "client", "build")));
+// if (process.env.NODE_ENV === "production") {
+//   const __dirname = path.resolve();
+//   app.use(express.static(path.join(__dirname, "client", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 8000;
 
